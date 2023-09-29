41fde5b727153db4a8df342080a6120324fe2898
jackzxliu <jacks.liu@mail.utoronto.ca>
2023-09-29 17:06:51 -0400

    test

--- A1/thread.c
+++ A1/thread.c
@@ -111,8 +111,6 @@ thread_create(void (*fn) (void *), void *parg)
         }
     }
 
-    printf("creating thread with tid %d\n", new_tid);
-
     // Allocate stack for new thread
     struct thread *new_thread = malloc(sizeof(struct thread));
 
