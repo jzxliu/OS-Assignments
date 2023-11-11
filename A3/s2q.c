edd67ae2f7b4cd2ee769a0a426eb838f68c64fff
jackzxliu <jacks.liu@mail.utoronto.ca>
2023-11-11 10:59:06 -0500

    commit message

--- A3/s2q.c
+++ A3/s2q.c
@@ -33,15 +33,15 @@ int s2q_evict(void)
  */
 void s2q_ref(int frame, vaddr_t vaddr)
 {
-    if (!(list_entry_is_linked(coremap[frame].framelist_entry))){ // It is not part of a queue yet
-        list_add_tail(&fifo_queue, &(coremap[frame].framelist_entry));
+    if (!(list_entry_is_linked(&coremap[frame].framelist_entry))){ // It is not part of a queue yet
+        list_add_tail(&fifo_queue, &coremap[frame].framelist_entry);
         fifo_size += 1;
         set_referenced(coremap[frame].pte, 0);
     } else if (get_referenced(coremap[frame].pte)) { // It has been referenced and moved to Am LRU queue
-        list_del(&(coremap[frame].framelist_entry));
+        list_del(&coremap[frame].framelist_entry);
         list_add_tail(&lru_queue, &coremap[frame].framelist_entry);
     } else { // Not yet referenced, should be moved from FIFO to LRU queue
-        list_del(&(coremap[frame].framelist_entry));
+        list_del(&coremap[frame].framelist_entry);
         list_add_tail(&lru_queue, &coremap[frame].framelist_entry);
         fifo_size -= 1;
         set_referenced(coremap[frame].pte, 1);
