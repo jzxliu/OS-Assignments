8202e9619a4b0a3245fd50e144b28977308442e8
jackzxliu <jacks.liu@mail.utoronto.ca>
2023-11-10 23:38:52 -0500

    commit message

--- A3/s2q.c
+++ A3/s2q.c
@@ -20,10 +20,10 @@ int s2q_evict(void)
     list_entry *entry;
     struct frame *to_evict;
     if (fifo_size > fifo_threshold){
-        entry = list_first_entry(fifo_queue);
+        entry = list_first_entry(&fifo_queue);
         fifo_size --;
     } else {
-        entry = list_first_entry(lru_queue);
+        entry = list_first_entry(&lru_queue);
     }
     to_evict = frame_of(entry);
     list_del(entry);
@@ -39,14 +39,14 @@ int s2q_evict(void)
 void s2q_ref(int frame, vaddr_t vaddr)
 {
     if (coremap[frame].framelist_entry.next == NULL){ // It is not part of a queue yet
-        list_add_tail(fifo_queue, &coremap[frame].framelist_entry);
+        list_add_tail(&fifo_queue, &coremap[frame].framelist_entry);
         fifo_size ++;
     } else if (get_referenced(coremap[frame].pte)) { // It has been referenced and moved to Am LRU queue
         list_del(&coremap[frame].framelist_entry);
-        list_add_tail(lru_queue, &coremap[frame].framelist_entry);
+        list_add_tail(&lru_queue, &coremap[frame].framelist_entry);
     } else { // Not yet referenced, should be moved from FIFO to LRU queue
         list_del(&coremap[frame].framelist_entry);
-        list_add_tail(lru_queue, &coremap[frame].framelist_entry);
+        list_add_tail(&lru_queue, &coremap[frame].framelist_entry);
         fifo_size --;
         set_referenced(coremap[frame].pte, 1);
     }
@@ -57,8 +57,8 @@ void s2q_ref(int frame, vaddr_t vaddr)
 /* Initialize any data structures needed for this replacement algorithm. */
 void s2q_init(void)
 {
-    list_init(lru_queue);
-    list_init(fifo_queue);
+    list_init(&lru_queue);
+    list_init(&fifo_queue);
     fifo_size = 0;
     fifo_threshold = memsize / 10;
 }
@@ -67,6 +67,6 @@ void s2q_init(void)
 void s2q_cleanup(void)
 {
     // I mean, technically this does nothing, but whatever
-    list_destroy(lru_queue);
-    list_destroy(fifo_queue);
+    list_destroy(&lru_queue);
+    list_destroy(&fifo_queue);
 }
